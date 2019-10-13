71a003a1268c5f380d85d2bbc991775ccbac6a61
phuong_nt <nguyenphuongbka@gmail.com>
2019-10-14 00:09:20 +0700

    ok

---
+++ Source/BasicPython.py
@@ -0,0 +1,14 @@
+# #string
+# data='hello world'
+# print(data[0])
+# print(len(data))
+# print(data)
+
+# #dictionary
+# myDict={"a":1,"b":2,"c":3}
+# print(myDict.keys())
+
+# #array numpy
+import numpy as np
+np.array([[1, 2], [3, 4]])
+
